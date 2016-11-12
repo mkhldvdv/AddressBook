243e08863d9c3851066b6a3d74347e8bda6e6752
mikhail davydov <mkhldvdv@gmail.com>
2016-11-13 01:09:38 +0300

    localization added

--- src/main/java/addressbook/configs/WebConfig.java
+++ src/main/java/addressbook/configs/WebConfig.java
@@ -7,12 +7,47 @@
 
 package addressbook.configs;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Created by mikhail.davydov on 04.11.2016.
  */
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        lci.setIgnoreInvalidLocale(true);
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
--- src/main/java/addressbook/controllers/AddressBookController.java
+++ src/main/java/addressbook/controllers/AddressBookController.java
@@ -8,12 +8,25 @@
 package addressbook.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * Created by mikhail.davydov on 21.10.2016.
  */
-@RestController
+@Controller
 @Slf4j
 public class AddressBookController {
+//    @RequestMapping("/")
+//    public String greeting(Model model) {
+//        return "index";
+//    }
 }
